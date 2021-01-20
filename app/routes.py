2f236ec2081d9757a1ac552372d321fccb1672fe
Wok <woctezuma@users.noreply.github.com>
2021-01-20 22:43:23 +0100

    Add route /find/<query_name>

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,16 @@
 from flask import jsonify
 
 from app import app
+from app.match_utils import find_most_similar_game_names
+
 
 @app.route("/")
 def main():
     return "Welcome to my app!"
 
-@app.route("/<name>")
-def match_game_name(name=None):
-    matches = get_matches(name)
-    return jsonify(matches)
 
+@app.route("/find/")
+@app.route("/find/<query_name>")
+def find_game_name(query_name=None, num_matches=5):
+    data = find_most_similar_game_names(query_name, num_matches)
+    return jsonify(data)
