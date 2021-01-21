6d0e2ec782198477244ae09a4fd548ff088fde8c
Wok <woctezuma@users.noreply.github.com>
2021-01-21 19:54:10 +0100

    Add rate limits

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,13 @@
 from flask import Flask
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 
 app = Flask(__name__)
+
+limiter = Limiter(
+    app,
+    key_func=get_remote_address,
+    default_limits=["25 per hour", "5 per minute"]
+)
 
 from app import routes
